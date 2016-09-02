96358451d892b0dcef2990c9eab88a6dee67298f
Nils-Axel Carlson <nils.carlson@my.southeastmn.edu>
2016-09-01 20:57:33 -0500

    Added ex 1d Bakery

---
+++ excersise1d1/Main Form.vb
@@ -0,0 +1,26 @@
+﻿'Name:          Bakery Project
+'Purpose:       Calculates the total number of items sold and sales
+'Programmer:    Nils Carlson on 09/01/16
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'preapare screen for the next sale
+        txtDonuts.Text = String.Empty
+        txtMuffins.Text = String.Empty
+        lblTotalItems.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+        txtDonuts.Focus()
+        'send the focus to the Donuts box
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        'calculate number of items sold and total sales
+        lblTotalItems.Text = Val(txtDonuts.Text) + Val(txtMuffins.Text)
+        lblTotalSales.Text = Val(lblTotalItems.Text) * 0.5
+        lblTotalSales.Text = Format(lblTotalSales.Text, "currency")
+    End Sub
+End Class
