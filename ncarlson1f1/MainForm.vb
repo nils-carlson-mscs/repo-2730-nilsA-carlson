2c11148357cf272144cc1cbfc505031203118b19
Nils-Axel Carlson <nils.carlson@my.southeastmn.edu>
2016-09-12 22:19:29 -0500

    excersise 1f1

---
+++ ncarlson1f1/MainForm.vb
@@ -0,0 +1,36 @@
+﻿Public Class frmMain
+
+    'Name:          Strawberry project
+    'Purpose:       Calculates the total number of items sold and sales
+    'Programmer:    Nils Carlson on 09/12/2016
+
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'calculate projected sales
+
+        lblProjStraw.Text = Format((1 + Val(txtProjIncrease.Text)) * Val(txtStraw.Text), "currency")
+        lblProjBlue.Text = Format((1 + Val(txtProjIncrease.Text)) * Val(txtBlue.Text), "currency")
+        lblProjRasp.Text = Format((1 + Val(txtProjIncrease.Text)) * Val(txtRasp.Text), "currency")
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' clear screen
+
+        txtStraw.Text = String.Empty
+        txtBlue.Text = String.Empty
+        txtRasp.Text = String.Empty
+        lblProjStraw.Text = String.Empty
+        lblProjBlue.Text = String.Empty
+        lblProjRasp.Text = String.Empty
+        txtProjIncrease.Text = String.Empty
+    End Sub
+End Class
